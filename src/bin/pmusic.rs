eeb689769349f9965250c6da381c8043f7e4347e
blchrd <thomas3.blanchard3@gmail.com>
2024-02-26 17:32:33 -0700

    Add a commented line with SheetMusicMaker implementation for generation

--- src/bin/pmusic.rs
+++ src/bin/pmusic.rs
@@ -1,7 +1,8 @@
 use std::time::{Duration, Instant};
 
 use pmusic::{
-    musicsource::{chord_music_maker::ChordMusicMaker, melody_music_maker::MelodyMusicMaker}, 
+    musicgeneration::sheet_generator::sheet_generation, 
+    musicsource::{chord_music_maker::ChordMusicMaker, melody_music_maker::MelodyMusicMaker, sheet_music_maker::SheetMusicMaker}, 
     musictheory::{chord_progression::ChordProgression, piano_key::PianoKey, scale::Scale}
 };
 use rodio::{dynamic_mixer, OutputStream, Sink, Source};
@@ -29,6 +30,7 @@ struct Opt {
 fn main() {
     let now = Instant::now();
     let opt = Opt::from_args();
+    let amplify_value = 0.2;
 
     let (controller, mixer) = dynamic_mixer::mixer::<f32>(2, 48_000);
     let (_stream, stream_handle) = OutputStream::try_default().unwrap();
@@ -42,15 +44,16 @@ fn main() {
             opt.tempo / 2
         );
         // By removing the .amplify at the end, we can make the sound saturate
-        controller.add(chords.take_duration(Duration::from_secs(opt.duration)).amplify(0.2));
+        controller.add(chords.take_duration(Duration::from_secs(opt.duration)).amplify(amplify_value));
     }
 
     let music = MelodyMusicMaker::new(opt.base_note, opt.scale, opt.octaves, opt.tempo);
+    // let music = SheetMusicMaker::new(sheet_generation(opt.base_note, opt.scale, opt.octaves), opt.tempo);
 
     if opt.file_out {
         let filepath = "./output/output.wav";
         println!("Export to {}", filepath);
-        controller.add(music.take_duration(Duration::from_secs(opt.duration)).amplify(0.2));
+        controller.add(music.take_duration(Duration::from_secs(opt.duration)).amplify(amplify_value));
         let head = wav_io::new_stereo_header();
         let mut file_out = std::fs::File::create(filepath).unwrap();
         wav_io::write_to_file(&mut file_out, &head, &mixer.convert_samples().into_iter().collect::<Vec<f32>>()).unwrap();
@@ -60,7 +63,7 @@ fn main() {
         println!("Execution took {} seconds.", elapsed_time.as_secs());
     } else {
         println!("{}", music);
-        controller.add(music.take_duration(Duration::from_secs(opt.duration)).amplify(0.2));
+        controller.add(music.take_duration(Duration::from_secs(opt.duration)).amplify(amplify_value));
         sink.append(mixer);
         sink.sleep_until_end();
     }
