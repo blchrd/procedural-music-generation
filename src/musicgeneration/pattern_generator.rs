df1aa8553ae1b50453d52581a8f66e036cfc3329
blchrd <thomas3.blanchard3@gmail.com>
2024-04-27 15:09:17 -0600

    Add max distance for notes between measures

--- src/musicgeneration/pattern_generator.rs
+++ src/musicgeneration/pattern_generator.rs
@@ -14,25 +14,29 @@ use super::rhythm_pattern_generator::rhythm_pattern_generation;
 pub fn pattern_generation(name: String, base_note: PianoKey, scale: Scale, octaves: u8, nb_measures: i32) -> Pattern {
     let mut pattern = Pattern::new(name);
     let max_distance = 5;
+    let max_distance_between_measures = 14;
 
     let mut seed = SmallRng::from_entropy();
     let keys = Key::new(scale, base_note, octaves).all_keys();
+    let mut measure_last_note: Option<PianoKey> = None;
 
     for _ in 0..nb_measures {
-        // Pick a finish number of note to make the pattern
-        // Try to implement some max distance between two measure, to make a sense of progression
         let rhythm_pattern = rhythm_pattern_generation(TimeSignature::default());
         let mut measure = Measure::new(TimeSignature::default());
         let mut prev_note: Option<PianoKey> = None;
         rhythm_pattern.iter().for_each(|value| {
             let mut note = *keys.iter().choose(&mut seed).unwrap();
-            while prev_note.is_some() && (note == prev_note.unwrap() || prev_note.unwrap().get_distance(note) > max_distance) {
+            while 
+                measure_last_note.is_some() && (note.get_distance(measure_last_note.unwrap()) > max_distance_between_measures) ||
+                prev_note.is_some() && (note == prev_note.unwrap() || prev_note.unwrap().get_distance(note) > max_distance) {
                 note = *keys.iter().choose(&mut seed).unwrap();
             }
+            measure_last_note = None;
             prev_note = Some(note.clone());
             measure.add_note(note, *value);
         });
         pattern.add_measure(measure);
+        measure_last_note = prev_note.clone();
     }
 
     pattern
